63864782fdf8686a446f2b49b9d9654059bcb556
dougJesus <douglas.jb@outlook.com.br>
2023-09-11 13:01:59 -0300

    menu navegação concluído

--- studio-designer/src/components/Nav.js
+++ studio-designer/src/components/Nav.js
@@ -1,9 +1,27 @@
 import styles from '../components/Nav.module.css'
+import Logo from '../images/Logo.svg'
 
 function Nav(){
     return(
-        <div className={styles.nav}>
-            <h1>aqui ficara o menu de navegação</h1>
+        <div>
+            <div className={styles.logo}>
+                <ul>
+                    <li>
+                        <img src={Logo}></img>
+                    </li>
+                </ul>
+            <div className={styles.nav}>
+                <ul>
+                    <li></li>
+                    <li>Home</li>
+                    <li>What We Do</li>
+                    <li>Service</li>
+                    <li>Project</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
